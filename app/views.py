aa41b866d5c34bd54371a394b17ade15caed82e8
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-29 08:06:58 -0700

    add persone view

--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .forms import PersonForm
 
-# Create your views here.
+class DemoException(Exception):
+    pass
+
+def simple_view(request):
+    if request.method == 'POST':
+        form = PersonForm()
+        context = {
+            'form' : form
+        }
+        raise DemoException('This is just a demo exce')
+        
+    return render(request, template_name="form.html", context=context )
