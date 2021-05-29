bad4881245b2598bd7586302e5699b2a0e151bd7
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-29 07:55:47 -0700

    add test model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Persone(models.Model):
+    name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField()
