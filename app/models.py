5974fbf1d53ef0602bc1c2652fbc0c816402db25
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-29 08:28:56 -0700

    fix typo bug in model name

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Persone(models.Model):
+class Person(models.Model):
     name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     email = models.EmailField()
