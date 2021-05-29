4c2d72f6ab13d6e96899018bfeeb5e1eef5551cd
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-29 09:24:48 -0700

    add process template

--- app/middleware.py
+++ app/middleware.py
@@ -2,10 +2,18 @@ class DemoMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
         self.num_exceptions = 0
+        self.context_response = {
+            "msg": {"warning": "Hello from context response !"}
+        }
 
     def __call__(self, request):
         print("MIDDLEWARE CALLED")
         response = self.get_response(request)
+        return response
+        
+    def process_template_response(self, request, response):
+        response.context_data['new_data'] = self.context_response
+        print(self.context_response)
         return response
 
     def process_view(self, request, view_func, view_args, view_kwargs):
