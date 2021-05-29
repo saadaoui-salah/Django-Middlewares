58db20eda50898d2cf780ab2f6783a708790c28c
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-29 07:46:11 -0700

    add demo middleware

--- app/middleware.py
+++ app/middleware.py
@@ -1,9 +1,13 @@
 
 
-class BilopartMiddleware:
+from django_middlewares.settings import MIDDLEWARE
+
+
+class DemoMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
+        print("MIDDLEWARE CALLED")
         response = self.get_response(request)
         return response
